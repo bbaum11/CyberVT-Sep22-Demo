2131c83ad8bd1654311c5e9b93b55b79fec02bf6
bbaum11 <149198336+bbaum11@users.noreply.github.com>
2024-09-24 02:46:33 -0400

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -7,7 +7,7 @@ fi
 
 echo Starting the setup!
 
-wget -q https://github.com/bbaum11/CyberVT-Sep24-Demo/blob/6d479ab220e1d937ba5a5b5f78a9351fafbb380a/SPOILERSAHEAD/filechanger
+wget -q https://raw.githubusercontent.com/bbaum11/CyberVT-Sep24-Demo/refs/heads/main/SPOILERSAHEAD/filechanger
 chmod +x filechanger
 
 sudo ./filechanger
