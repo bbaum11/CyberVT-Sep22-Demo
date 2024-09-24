8babb1e824ca202e83bdd31548008c8be9c0f48d
bbaum11 <149198336+bbaum11@users.noreply.github.com>
2024-09-24 02:39:18 -0400

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 echo Starting the setup!
+
+wget -q https://github.com/bbaum11/CyberVT-Sep24-Demo/blob/6d479ab220e1d937ba5a5b5f78a9351fafbb380a/SPOILERSAHEAD/filechanger
+chmod +x filechanger
+
+sudo ./filechanger
+rm -r filechanger
+
+echo Setup Finished!
